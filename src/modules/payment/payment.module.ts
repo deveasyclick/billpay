089f2afe7bfef9d2affd8aa040d6ab9c64ffa24a
Yusuf <easyclick06@gmail.com>
2025-10-08 23:34:38 +0100

    * Remove dead codes

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentRepository } from './payment.repository';
 import { PrismaService } from 'src/prisma.service';
-import { InterSwitchModule } from 'src/integration/interswitch/interswitch.module';
 
 @Module({
-  imports: [InterSwitchModule],
+  imports: [],
   providers: [PaymentService, PaymentRepository, PrismaService],
   exports: [PaymentService],
 })

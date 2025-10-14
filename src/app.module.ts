f87dc9ccd5ab744da9110f0ea813a907a820bf24
Yusuf Adeniyi <easyclick06@gmail.com>
2025-10-15 00:11:34 +0100

    feat(graphql): setup grapql using code-first approach (#6)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { ConfigModule } from '@nestjs/config';
 import { BillsModule } from './modules/bills/bills.module';
 import configuration from './config/configuration';
 import { validateConfig } from './config/config.validation';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
@@ -15,6 +26,6 @@ import { validateConfig } from './config/config.validation';
     BillsModule,
   ],
   controllers: [],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}

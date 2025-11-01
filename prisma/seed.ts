6f4fcefb970e56a6e4b3798721d40571f90b5928
Yusuf Adeniyi <easyclick06@gmail.com>
2025-11-01 15:36:57 +0100

    Feat/update prisma schema (#8)
    * feat(prisma): update db and migration to support multi-provider
    
    * chore: add .env.example
    
    ---------
    
    Co-authored-by: Yusuf <easyclick05@gmail.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,51 @@
-import { PrismaService } from 'src/prisma.service';
+import { BillCategory, Prisma, PrismaClient, Providers } from '@prisma/client';
 
-const prisma = new PrismaService();
+const prisma = new PrismaClient();
+
 async function main() {
-  console.log('iniside seed');
-  await prisma.provider.create({
-    data: {
-      name: 'interswitch',
-      active: true,
-    },
-  });
+  console.log('Seeding database...');
+
+  // Example: seed providers
+  const providersData: Prisma.BillingProviderCreateInput[] = [
+    { name: Providers.INTERSWITCH, isActive: true },
+    { name: Providers.VTPASS, isActive: true },
+  ];
+
+  await Promise.all(
+    providersData.map(async (p) => {
+      return prisma.billingProvider.upsert({
+        where: { name: p.name },
+        update: {},
+        create: p,
+      });
+    }),
+  );
+
+  // Example: seed categories
+  const categoriesData: Prisma.BillingCategoryCreateInput[] = [
+    { name: BillCategory.AIRTIME, dynamic: false },
+    { name: BillCategory.ELECTRICITY, dynamic: false },
+    { name: BillCategory.GAMING, dynamic: false },
+    { name: BillCategory.DATA, dynamic: true },
+    { name: BillCategory.TV, dynamic: true },
+  ];
+
+  for (const category of categoriesData) {
+    await prisma.billingCategory.upsert({
+      where: { name: category.name },
+      update: {},
+      create: category,
+    });
+  }
+
+  console.log('Seeding complete!');
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
+  .catch((e) => {
+    console.error('Seed failed:', e);
     process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
   });

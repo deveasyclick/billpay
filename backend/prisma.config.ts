3716ca04f4c8013256ea771cea651f7516220327
Yusuf <easyclick05@gmail.com>
2025-11-20 10:42:32 +0100

    remove console.log

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
-console.log('prisma', env('DB_URL'));
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
